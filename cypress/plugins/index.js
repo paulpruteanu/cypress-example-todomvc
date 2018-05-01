ce6804e6ee31c9c057dd043bfdae0e195b695e91
Paul Pruteanu <paul.pruteanu@lloydsbanking.com>
2018-05-01 12:58:27 +0100

    showcasing preprocessor resolving the source file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,19 +1,8 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/* eslint-disable no-unused-vars */
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+/* eslint-disable no-unused-vars, no-undef */
+module.exports = (on) => {
+  on('file:preprocessor', (file) => Promise.resolve(file.filePath))
+  /* "cypress run" (default to electron) throws this error: Uncaught ReferenceError: require is not defined
+  it works resolving file.outputPath, but it's not good enough for staged containers that doesn't have the outputPath
+  at the time */
 }
 /* eslint-enable no-unused-vars */
